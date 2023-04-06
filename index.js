0d34d1d2d4d18b585594f970e81d116072090a41
Kevin Bravo <kbravo1024@gmail.com>
2023-04-05 21:48:03 -0700

    complete elements lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const main = document.querySelector("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute("id", "victory");
+
+
+newHeader.textContent = "Kevin is the champion";
